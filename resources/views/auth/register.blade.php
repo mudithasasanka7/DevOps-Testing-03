e5621dc36d6c6e0672853fe2e843183ba0f62cb4
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2025-05-02 00:34:13 +0530

    Edit commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
     <h2>Register</h2>
     <form action="{{route('register')}}" method="post">
         @csrf
-        <label for="name">Name</label>
+        <label for="name">Enter Name</label>
         <input type="text" name="name" id="name" required>
         <label for="email">Email</label>
         <input type="email" name="email" id="email" required>
